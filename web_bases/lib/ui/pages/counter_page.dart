bc370b26b153f6592a9798c7d81c438846c3c239
CesarArellano <raywayday@gmail.com>
2022-01-20 22:52:23 -0600

    Adding counter in CounterPage

---
+++ web_bases/lib/ui/pages/counter_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+class CounterPage extends StatefulWidget {
+
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Contador: $counter',
+            style: const TextStyle( fontSize: 80, fontWeight: FontWeight.bold ),
+          ),
+
+          TextButton(
+            child: const Text('Incrementar'),
+            onPressed: () {
+              setState(() => counter++ );
+            },
+          )
+        ],
+      )
+    );
+  }
+}
