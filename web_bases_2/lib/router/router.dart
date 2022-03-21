a62ca2d0cf6bdfc44b5bfcc66e0123aeac1fd0ae
CesarArellano <raywayday@gmail.com>
2022-03-21 00:00:10 -0600

    Added new route to page not found

--- web_bases_2/lib/router/router.dart
+++ web_bases_2/lib/router/router.dart
@@ -1,6 +1,8 @@
 
 import 'package:fluro/fluro.dart';
+import 'package:web_bases/ui/views/counter_provider_view.dart';
 import 'package:web_bases/ui/views/counter_view.dart';
+import 'package:web_bases/ui/views/error_view.dart';
 
 class Flurorouter {
   static final FluroRouter router = FluroRouter();
@@ -8,12 +10,35 @@ class Flurorouter {
   static void configureRoute() {
     router.define(
       '/',
-      handler: _counterHandler
-  ) ;
+      handler: _counterHandler,
+      transitionType: TransitionType.fadeIn
+    );
+
+    router.define(
+      '/stateful',
+      handler: _counterHandler,
+      transitionType: TransitionType.fadeIn
+    );
+
+    router.define(
+      '/provider',
+      handler: _providerCounterHandler,
+      transitionType: TransitionType.fadeIn
+    );
+
+    router.notFoundHandler = _pageNotFoundHandler;
   }
 
   // Handlers
   static final Handler _counterHandler = Handler(
     handlerFunc: ( _, __) => const CounterView()
+  );
+
+  static final Handler _providerCounterHandler = Handler(
+    handlerFunc: ( _, __) => const CounterProviderView()
+  );
+
+  static final Handler _pageNotFoundHandler = Handler(
+    handlerFunc: ( _, __) => const ErrorView()
   );
 }
