44db15273170a3d3a866783b93cc74494842d77e
CesarArellano <raywayday@gmail.com>
2022-03-27 21:11:48 -0600

    Added router and handlers

--- flutter_landing_page/lib/main.dart
+++ flutter_landing_page/lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_landing_page/ui/screens/home_screen.dart';
 
-void main() => runApp( const MyApp() );
+import 'package:flutter_landing_page/router/router.dart';
+
+void main() {
+  Flurorouter.configureRoutes(); 
+  runApp( const MyApp() );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: HomeScreen()
+      title: 'Home Page',
+      onGenerateRoute: Flurorouter.router.generator,
     );
   }
 }
--- flutter_landing_page/lib/ui/pages/home_page.dart
+++ flutter_landing_page/lib/ui/pages/home_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomePage extends StatelessWidget {
   
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('HomeScreen'),
+        child: Text('Home Page'),
       ),
     );
   }
