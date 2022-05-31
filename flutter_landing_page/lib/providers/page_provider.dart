c1122867c8133b4e7aed5c5731904c7e86ccc91b
CesarArellano <raywayday@gmail.com>
2022-05-30 23:32:16 -0500

    Changed routeName in URL

--- flutter_landing_page/lib/providers/page_provider.dart
+++ flutter_landing_page/lib/providers/page_provider.dart
@@ -1,10 +1,16 @@
+// import 'dart:html' as html;
+
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
   
   PageController scrollController = PageController();
+  final List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
 
   void goTo(int index) {
+    final routeName = _pages[index];
+    html.window.history.pushState(null, 'none', '#/$routeName');
     scrollController.animateToPage(
       index,
       duration: const Duration(milliseconds: 200), 
