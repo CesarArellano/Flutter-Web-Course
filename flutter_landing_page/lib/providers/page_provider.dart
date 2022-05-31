1ac496448f41c34e9bb8bd95c64b79aa52d03450
CesarArellano <raywayday@gmail.com>
2022-05-30 23:45:50 -0500

    Added createScrollController function

--- flutter_landing_page/lib/providers/page_provider.dart
+++ flutter_landing_page/lib/providers/page_provider.dart
@@ -8,6 +8,17 @@ class PageProvider extends ChangeNotifier {
   PageController scrollController = PageController();
   final List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
 
+  void createScrollController(String routeName) {
+    scrollController = PageController(
+      initialPage: getPageIndex(routeName)
+    );
+  }
+  
+  int getPageIndex(String routeName) {
+    final index = _pages.indexOf(routeName);
+    return (index == -1) ? 0 : index;
+  }
+
   void goTo(int index) {
     final routeName = _pages[index];
     html.window.history.pushState(null, 'none', '#/$routeName');
--- flutter_landing_page/lib/router/route_handlers.dart
+++ flutter_landing_page/lib/router/route_handlers.dart
@@ -1,11 +1,15 @@
 
 import 'package:fluro/fluro.dart';
+import 'package:flutter_landing_page/providers/page_provider.dart';
 import 'package:flutter_landing_page/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 final homeHandler = Handler(
   handlerFunc: ( context, params ) {
     final page = params['page']!.first;
+    final pageProvider = Provider.of<PageProvider>(context!, listen: false);
     if( page != '/') {
+      pageProvider.createScrollController(page);
       return const HomePage();
     }
   }
