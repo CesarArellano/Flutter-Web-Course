148d20311697e193a9c54edbf6a73f6f20ea49fc
CesarArellano <raywayday@gmail.com>
2022-06-13 23:18:07 -0500

    Added DesktopBody Widget

--- admin_dashboard/lib/router/admin_handlers.dart
+++ admin_dashboard/lib/router/admin_handlers.dart
@@ -1,10 +1,11 @@
-import 'package:admin_dashboard/ui/views/login_view.dart';
 import 'package:fluro/fluro.dart';
+
+import '../ui/layouts/auth/auth_layout.dart';
 
 class AdminHandlers {
   static Handler login = Handler(
     handlerFunc:(context, parameters) {
-      return const LoginView();
+      return const AuthLayout();
     },
   );
 }
--- admin_dashboard/lib/ui/layouts/auth/auth_layout.dart
+++ admin_dashboard/lib/ui/layouts/auth/auth_layout.dart
@@ -6,9 +6,43 @@ class AuthLayout extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AuthLayout'),
+    return Scaffold(
+      body: ListView(
+        children: const [
+          // Desktop
+          _DesktopBody()
+          // Mobile
+          // LinksBar
+        ],
+      )
+    );
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.black,
+      child: Row(
+        children: [
+          // Twitter Background
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+
+          const SizedBox(
+            width: 600,
+            height: double.infinity,
+          )
+        ],
       ),
     );
   }
